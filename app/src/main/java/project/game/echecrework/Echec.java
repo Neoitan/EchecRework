74934600caf187887715df95d9a555e5b6356d3d
Neoitan <41125124+Neoitan@users.noreply.github.com>
2020-01-09 18:09:35 +0100

    Initial commit

---
+++ app/src/main/java/project/game/echecrework/Echec.java
@@ -0,0 +1,283 @@
+package project.game.echecrework;
+
+import android.util.Log;
+
+class Echec {
+
+    /*CONSTANTES*/
+
+    private final int TAILLEX = 8;
+    private final int TAILLEY = 8;
+
+    /* ATTRIBUTS*/
+
+    private boolean isPlaying = true;
+
+    // 1 -> B
+    // 2 -> N
+    private int whoPlay = 1;
+
+    // déclaration du plateau
+    private String[][] plateau;
+
+    // Sauvegarder la position des rois
+    int xKB, xKN, yKB, yKN;
+
+    // Constructeur
+    public Echec(){
+        this.plateau = new String[TAILLEX][TAILLEY];
+        initPlateau();
+        Log.i("Affichage_board", toString());
+    }
+
+    // Initialise le plateau avec les pièces au bonne coordonnées.
+    // Une case qui n'a pas de pièce est représenté par un "V." qui est une image vide.
+    public void initPlateau(){
+        this.isPlaying = true;
+        this.whoPlay = 1;
+
+        for(int i=0; i < this.TAILLEX; i++){
+            for(int k=0; k < this.TAILLEY; k++) {
+                this.plateau[i][k] = "V.";
+            }
+        }
+
+        this.xKN = 0;
+        this.yKN = 4;
+
+        this.xKB = 7;
+        this.yKB = 4;
+
+        this.plateau[0][0] = "NR";
+        this.plateau[0][1] = "NH";
+        this.plateau[0][2] = "NB";
+        this.plateau[0][3] = "NC";
+        this.plateau[0][4] = "NK";
+        this.plateau[0][5] = "NB";
+        this.plateau[0][6] = "NH";
+        this.plateau[0][7] = "NR";
+
+        this.plateau[1][0] = "NP";
+        this.plateau[1][1] = "NP";
+        this.plateau[1][2] = "NP";
+        this.plateau[1][3] = "NP";
+        this.plateau[1][4] = "NP";
+        this.plateau[1][5] = "NP";
+        this.plateau[1][6] = "NP";
+        this.plateau[1][7] = "NP";
+
+
+        this.plateau[7][0] = "BR";
+        this.plateau[7][1] = "BH";
+        this.plateau[7][2] = "BB";
+        this.plateau[7][3] = "BC";
+        this.plateau[7][4] = "BK";
+        this.plateau[7][5] = "BB";
+        this.plateau[7][6] = "BH";
+        this.plateau[7][7] = "BR";
+
+        this.plateau[6][0] = "BP";
+        this.plateau[6][1] = "BP";
+        this.plateau[6][2] = "BP";
+        this.plateau[6][3] = "BP";
+        this.plateau[6][4] = "BP";
+        this.plateau[6][5] = "BP";
+        this.plateau[6][6] = "BP";
+        this.plateau[6][7] = "BP";
+    }
+
+    // Fonction de déplacement
+    // Les vérifications y sont comprisent.
+    public boolean move(int x, int y, int x1, int y1){
+
+        String token = getCase(x, y);
+        boolean deplacement = false;
+
+        if((x == x1 && y == y1) || ( getCase(x, y).charAt(0) == getCase(x1, y1).charAt(0) ) || getCase(x, y).charAt(0) != this.getPlayer().charAt(0) ) {
+        }else
+        /* REGLE DEPLACEMENT */
+            switch(token.charAt(1)){
+                /* PAWN */
+                case 'P':
+                    if(token.charAt(0) == 'N' ){
+                        /*
+                        * déplacement de 2 si position = 1 / 6
+                        * déplacement de 1 sinon
+                        * déplacement de diagonal si pion à attraper
+                        *
+                        *
+                        * */
+                        if(
+                                x<7 &&
+                                        ( (y == y1) && x == (x1 - 1) && getCase(x1, y1).charAt(0)=='V' ) ||
+                                        ( (x == 1) && (y == y1) && ( x == (x1 - 2) ) ) ||
+                                        ( (x == (x1 - 1)) && (Math.abs(y - y1) == 1) && getCase(x1, y1).charAt(0)!='V' )
+
+
+                        ) deplacement = true;
+                    }
+                    else {
+                        if(
+                                x>0 &&
+                                        ( (y == y1) && x == (x1 + 1) && getCase(x1, y1).charAt(0)=='V' ) ||
+                                        ( (x == 6) && (y == y1) && ( x == (x1 + 2) ) ) ||
+                                        ( (x == (x1 + 1)) && (Math.abs(y - y1) == 1) && getCase(x1, y1).charAt(0)!='V' )
+
+
+                        ) deplacement = true;
+                    }
+                    break;
+                /* ROOK */
+                case 'R':
+                    if( !((x != x1) && (y != y1)) ){
+                        deplacement = true;
+                    }
+                    break;
+                /* HORSE */
+                case 'H':
+                    if( ( Math.abs( x - x1 ) == 2 && Math.abs( y - y1 ) == 1 ) || ( Math.abs( x - x1 ) == 1  && Math.abs( y - y1 ) == 2 ) ){
+                        deplacement = true;
+                    }
+                    break;
+                /* BISHOP */
+                case 'B':
+                    if( Math.abs(x - x1) == Math.abs(y - y1) ){
+                        deplacement = true;
+                    }
+                    break;
+                /* CROWN */
+                case 'C':
+                    if( !((x != x1) && (y != y1)) || ( Math.abs(x - x1) == Math.abs(y - y1) ) ){
+                        deplacement = true;
+                    }
+                    break;
+                /* KING */
+                case 'K':
+                    if( Math.abs(x - x1) <= 1|| Math.abs(y - y1) <= 1 ){
+                        deplacement = true;
+                    }
+                    break;
+            }
+
+            boolean temp = true;
+
+
+
+            if( deplacement ){
+                //Verification que la pièce déplacée de saute pas d'autres pièces
+                //Verticaux et Horizontaux
+                if(y==y1){
+                    if(x > x1)
+                        for(int i=x-1; i>x1; i--) {
+                            if (!getCase(i, y).equals("V.")) deplacement = false;
+                        }
+                    else
+                        for( int i=x+1; i<x1; i++) {
+                            if (!getCase(i, y).equals("V.")) deplacement = false;
+                        }
+                }
+                else if(x == x1){
+                    if(y > y1)
+                        for(int i=y-1; i>y1; i--) {
+                            if (!getCase(x, i).equals("V.")) deplacement = false;
+                        }
+                    else
+                        for( int i=y+1; i<y1; i++) {
+                            if (!getCase(x, i).equals("V.")) deplacement = false;
+                        }
+                }
+                //Diagonales
+                if( x<x1 && y<y1 ){
+                    for( int i=x+1, j=y+1; i<x1; i++, j++){
+                        if (!getCase(i, j).equals("V.")) deplacement = false; Log.i("Test", "1");
+                    }
+                }
+                else if( x<x1 && y>y1 ){
+                    for( int i=x+1, j=y-1; i<x1; i++, j--){
+                        if (!getCase(i, j).equals("V.")) deplacement = false; Log.i("Test", "2");
+                    }
+                }
+                else if( x>x1 && y<y1 ){
+                    for( int i=x-1, j=y+1; i>x1; i--, j++){
+                        if (!getCase(i, j).equals("V.")) deplacement = false; Log.i("Test", "3");
+                    }
+                }
+                else if( x>x1 && y>y1 ){
+                    for( int i=x-1, j=y-1; i>x1; i--, j--){
+                        if (!getCase(i, j).equals("V.")) deplacement = false; Log.i("Test", "4");
+                    }
+                }
+
+                // Si le déplacement est correct on l'effectue.
+                if(deplacement) {
+                    if(getCase(x, y).equals("NK")){
+                        this.xKN = x1;
+                        this.yKN = y1;
+                    }
+                    else if(getCase(x, y).equals("BK")){
+                        this.xKB = x1;
+                        this.yKB = y1;
+                    }
+                    this.plateau[x1][y1] = this.plateau[x][y];
+                    this.plateau[x][y] = "V.";
+                }
+            }
+            return deplacement;
+    }
+
+    //Affichage en mode console.
+    public String toString() {
+        String s = "      1  2  3  4  5  6  7  8";
+
+
+        s += "\n     --------------------\n1   |";
+
+        for(int i=0; i < this.TAILLEX; i++){
+            for(int k=0; k < this.TAILLEY; k++){
+                s += this.plateau[i][k] + "|";
+            }
+            if(i<7) s += "\n     --------------------\n" + (i+2) + "   |";
+            else    s += "\n     --------------------\n";
+        }
+
+        return s;
+    }
+
+    // Passe le tour d'un joueur à l'autre.
+    public void changePlayer(){
+        if (this.whoPlay == 1) this.whoPlay += 1; else this.whoPlay -= 1;
+    }
+
+    // Récupère le joueur qui joue actuellement.
+    public String getPlayer(){
+        if(this.whoPlay == 1) return "Blanc"; else return "Noir";
+    }
+
+    // Récupère la valeur d'une case.
+    // La valeur étant la pièce (ou non) qui l'occupe.
+    public String getCase(int x, int y) {
+        return this.plateau[x][y];
+    }
+
+    // Vérification de la situation d'échec et échec et mat
+    public boolean complete() {
+
+        /*for(int i = 0; i < this.TAILLEX; i++){
+            for(int j = 0; j < this.TAILLEX; j++) {
+                switch(getCase(i, j)){
+                    case "BP":
+
+                        break;
+
+                }
+            }
+        }*/
+
+        if( getCase( this.xKB-1, this.yKB-1 ).equals("NB") ){
+            isPlaying = !isPlaying;
+        }
+
+        //if( this.plateau[5][0].equals("BP") ) isPlaying = false;
+        return !isPlaying;
+    }
+}
---
+++ app/src/main/java/project/game/echecrework/MainActivity.java
@@ -0,0 +1,222 @@
+package project.game.echecrework;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Echec board;
+
+    private ImageButton IDBoard[][];
+    private int idButton;
+
+    private Button bReset;
+    private TextView tvPlayer;
+
+    private int nbTurn = 1;
+
+    /*SAVE PLAY*/
+
+    int tokens[] = {-1, -1, -1, -1};
+
+    private boolean firstTouch = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        this.board = new Echec();
+        this.bReset = findViewById(R.id.bReset);
+        this.bReset.setOnClickListener(this);
+
+        this.tvPlayer = findViewById(R.id.tvPlayer);
+
+        initBoard();
+    }
+
+    private void initBoard() {
+        this.IDBoard = new ImageButton[8][8];
+        this.idButton = 0;
+        /*CREATION DU LAYOUT*/
+
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.TLBoard);
+        tableLayout.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+        /* CREATION DE LA ROW */
+        for(int i=0; i < 8; i++){
+            final TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+            /* CREATION DU IMAGEBUTTON */
+            for (int k = 0; k < 8; k++) {
+                // Creation  button
+                final ImageButton button = new ImageButton(this);
+
+                Log.i("CHECK", "Check image" + this.idButton);
+
+                button.setLayoutParams(new TableRow.LayoutParams(115, 115));
+                button.setId(this.idButton++);
+
+                button.setOnClickListener(this);
+
+                // SAVE ID
+                this.IDBoard[i][k] = button;
+                // ADD IN ROW
+                tableRow.addView(button);
+            }
+            // ADD IN LAYOUT
+            tableLayout.addView(tableRow);
+        }
+        majGame();
+    }
+
+    // Mise à jour du plateau
+    private void majButton() {
+        for(int i=0; i < 8; i++) {
+            for (int k = 0; k < 8; k++) {
+                ImageButton button = this.IDBoard[i][k];
+                switch (board.getCase(i, k)) {
+                    case "V.":
+                        button.setImageResource(R.drawable.vide);
+                        break;
+
+                    /* PION BLANC */
+                    case "BR":
+                        button.setImageResource(R.drawable.b_rook);
+                        break;
+                    case "BH":
+                        button.setImageResource(R.drawable.b_horse);
+                        break;
+                    case "BB":
+                        button.setImageResource(R.drawable.b_bishop);
+                        break;
+                    case "BC":
+                        button.setImageResource(R.drawable.b_crown);
+                        break;
+                    case "BK":
+                        button.setImageResource(R.drawable.b_king);
+                        break;
+                    case "BP":
+                        button.setImageResource(R.drawable.b_pawn);
+                        break;
+
+                    /* PION NOIR */
+                    case "NR":
+                        button.setImageResource(R.drawable.n_rook);
+                        break;
+                    case "NH":
+                        button.setImageResource(R.drawable.n_horse);
+                        break;
+                    case "NB":
+                        button.setImageResource(R.drawable.n_bishop);
+                        break;
+                    case "NC":
+                        button.setImageResource(R.drawable.n_crown);
+                        break;
+                    case "NK":
+                        button.setImageResource(R.drawable.n_king);
+                        break;
+                    case "NP":
+                        button.setImageResource(R.drawable.n_pawn);
+                        break;
+                }
+            }
+        }
+    }
+
+    /* POUR RECUPERER LES COORD X ET Y DEPUIS L'ID DE LA LISTE*/
+    private int[] searchIndex(int id){
+        for( int i = 0; i < 8; i++){
+            for( int k = 0; k < 8; k++) {
+                if (this.IDBoard[i][k].getId() == id){
+                    int tabId[] = {i, k};
+                    return tabId;
+                }
+            }
+        }
+        return null;
+    }
+
+    // Fonction d'évènement de click
+    @Override
+    public void onClick(View v) {
+
+        if(v == this.bReset) reset();
+        else {
+            Log.i("COORD", v.getId()+"\n"+searchIndex(v.getId())[0]+"-"+searchIndex(v.getId())[1] + "\n" + this.board.getCase(searchIndex(v.getId())[0], searchIndex(v.getId())[1]));
+            if(this.firstTouch && !this.board.getCase(searchIndex(v.getId())[0], searchIndex(v.getId())[1]).equals("V.")) {
+
+                    this.tokens[0] = searchIndex(v.getId())[0];
+                    this.tokens[1] = searchIndex(v.getId())[1];
+                    this.firstTouch = false;
+
+            }
+            else if(!this.firstTouch){
+                this.tokens[2] = searchIndex(v.getId())[0];
+                this.tokens[3] = searchIndex(v.getId())[1];
+                this.firstTouch = true;
+
+                if( board.move(tokens[0], tokens[1], tokens[2], tokens[3]) ) {
+                    this.board.changePlayer();
+                    this.nbTurn++;
+                }
+                this.tokens[0] = -1;
+                this.tokens[1] = -1;
+                this.tokens[2] = -1;
+                this.tokens[3] = -1;
+
+            }
+        }
+
+        majGame();
+    }
+
+    // Fonction de mise à jour principale
+    public void majGame(){
+        majButton();
+        majIHM();
+        verif();
+    }
+
+    // Mise à jour de l'IHM donnant le nom du joueur actuel
+    public void majIHM(){
+        this.tvPlayer.setText("C'est au joueur " + this.board.getPlayer());
+    }
+
+    // Fonction de remise à zero du plateau
+    public void reset(){
+        this.nbTurn = 1;
+        this.board.initPlateau();
+        majGame();
+    }
+
+    // Vérification de la situation d'échec et échec et mat grâce à la fonction de la classe Echec
+    public void verif(){
+        if(this.board.complete()){
+            final CustomPopup customPopup = new CustomPopup(this);
+            customPopup.setTitle("Fin de partie !");
+            customPopup.setImageWin(R.drawable.b_pawn);
+            customPopup.setSubTitle("Le joueur " + "X" + " à gagné !");
+            customPopup.getResetButton().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "Nouvelle partie ...", Toast.LENGTH_LONG);
+                    customPopup.dismiss();
+                    reset();
+                }
+            });
+            customPopup.build();
+        }
+    }
+}
